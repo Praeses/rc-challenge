81f7eee5064ef4e19b569d83550cdfdaa544eb51
andrew <roan.util@gmail.com>
2016-06-21 23:42:40 -0500

    2016.6.21 xps nonfunctional

--- src/main/java/com/praeses/interviews/rcchallenge/Main.java
+++ src/main/java/com/praeses/interviews/rcchallenge/Main.java
@@ -3,6 +3,7 @@ package com.praeses.interviews.rcchallenge;
 import java.io.*;
 import java.util.*;
 import java.lang.System.*;
+import java.lang.Integer;
 
 public class Main {
 
@@ -18,105 +19,240 @@ class Car {
 	public int carX = 0;
 	public int carY = 0;
 	public String carDir = "north";
-	public String startXString = "Please input the starting X Coordinate:";
-	public String startYString = "Please input the starting Y Coordinate:";
-	public String startDirString = "Please input the starting direction:";
-	public String intError = "Please enter an integer!";
-	public String dirError = "Must be north, south, east, or west!";
-	public Scanner userInput;
+	int carGear = 1;
+	int[] gearBox = {-1,1};
+	String startXString = "Please input the starting X Coordinate:";
+	String startYString = "Please input the starting Y Coordinate:";
+	String startDirString = "Please input the starting direction:";
+	String intError = "Please enter an integer!";
+	String dirError = "Must be north, south, east, or west!";
+	String badCom = "Unrecognized Command. Type 'help' for guidance.";
+	Scanner scInput;
 
 	Car() {
 		
-		//userInput = new Scanner(System.in);
-		
-		carStart();
+		ignition();
+		driverSeat();
 		
 	}
 	
-	private void carStart() {
+	private void ignition() {
 		
-		private boolean loop = true;
+		boolean loop = true;
 		
-		userInput = new Scanner(System.in);
+		scInput = new Scanner(System.in);
 		
 		do {
 			try {
-				System.out.print(startXString);
-				carX = userInput.nextInt();
+				System.out.println(startXString);
+				carX = scInput.nextInt();
 				loop = false;
    			} catch (Exception e) {
-   					System.out.print(intError);
-   					userInput.nextLine();
+   					System.out.println(intError);
+   					scInput.nextLine();
    			}
    		} while (loop == true);
    		loop = true;
    		
    		do {
 			try {
-				System.out.print(startYString);
-				carY = userInput.nextInt();
+				System.out.println(startYString);
+				carY = scInput.nextInt();
 				loop = false;
    			} catch (Exception e) {
-   					System.out.print(intError);
-   					userInput.nextLine();
+   					System.out.println(intError);
+   					scInput.nextLine();
    			}
    		} while (loop == true);
    		loop = true;
    		
    		do {
-			try {
-				System.out.print(startDirString);
-				carDir = userInput.next();
-				switch (carDir) {
-					case "north": case "south": case "east": case "west": 
-					loop = false;
-				}
-   			} catch (Exception e) {
-   					System.out.print(dirError);
-   					userInput.nextLine();
+   			try {
+			System.out.println(startDirString);
+			carDir = scInput.next();
+			switch (carDir) {
+				case "north": case "south": case "east": case "west": 
+				loop = false;
+			}
+			} catch (Exception e) {
+   				System.out.println(dirError);
+   				scInput.nextLine();
    			}
    		} while (loop == true);
    		
-   		userInput.close();
+   		scInput.close();
 
 	}
 	
-	private void pilotSeat() {
+	private void driverSeat() {
 	
-		private boolean loop = true;	
-		private String drivingGuide = "TBD";
-		private String scInput;
-	
-		userInput = new Scanner(System.in);
+		boolean loop = true;
+		boolean loopB = true;	
+		String drivingGuide = "TBD";
+		String delimit = "[,]";
+		String userInput = "init";
+		String[] userInputArray;	
+		int arrayLength;
+		int temp;
 		
-		while (scInput != "exit") {
+		scInput = new Scanner(System.in);
 		
+		do {
 			do {
 				try {
+				System.out.println("Drive!");
+				userInput = scInput.nextLine();
+				System.out.println("test");
+				loop = false;
+				} catch (Exception e) {
+				System.out.print("error");
+				scInput.nextLine();
+				}
+				
+			} while (loop == true);
+			
+			userInputArray = userInput.split(delimit);		
+			arrayLength = userInputArray.length;
+		
+			for (int i = 0; i < arrayLength; i++) {
+				userInputArray[i] = userInputArray[i].trim();
+			}
+		
+		
+			if (arrayLength == 3) {				
+				
+				if (isInteger(userInputArray[1], 10) == true && isInteger(userInputArray[2], 10) == true) {
+				
+					steeringWheel(userInputArray[0]);
+					transmission(Integer.parseInt(userInputArray[1]));
+					gasPedal(Integer.parseInt(userInputArray[2]));
+					
+				} else {
+				
+					System.out.println(badCom);
+				
+				}
+				
+			} else if (arrayLength == 2) {
+			
+				if (userInputArray[0] == "gear" && isInteger(userInputArray[1],10)) {
+				
+					transmission(Integer.parseInt(userInputArray[1]));
+					
+				} else {
+				
+					System.out.println(badCom);
+				
+				}
+				
+			} else if (arrayLength == 1) {
+			
+				if (userInputArray[0] == "north" || userInputArray[0] == "south" || userInputArray[0] == "east" || userInputArray[0] == "west") {
+				
+					steeringWheel(userInputArray[0]);
+					
+				} else if (isInteger(userInputArray[0], 10) == true) {
+				
+					gasPedal(Integer.parseInt(userInputArray[0]));
+					
+				} else if (userInputArray[0] == "help") {
+				
 					System.out.print(drivingGuide);
-					scInput = userInput.next();
-					switch (scInput) {
-						case "north": case "south": case "east": case "west": 
-						steeringWheel(scInput);
-						loop = false;
-						case "help":
-						System.out.print(drivingGuide);
-						loop = false;
-					}
-   				} catch (Exception e) {
-   					System.out.print("Error");
-   					userInput.nextLine();
-   				}
-   			} while (loop == true);
-   			loop = true;
-   		
-   		}
+					
+				} else if (userInputArray[0] == "exit") {
+				
+					loopB = false;
+					
+				} else if (userInputArray[0] == "around the block") {
+				
+				} else if (userInputArray[0] == "find obstacle") {
+			
+				} else {
+				
+					System.out.println(badCom);
+				
+				}
+				
+			} else {
+			
+				System.out.println(badCom);
+			
+			}
+			
+		} while (loopB == true);
+		
+		scInput.close();
 	
 	}
 	
 	private void steeringWheel(String dir) {
 	
 		carDir = dir;
+		
+		System.out.println("Car is now facing " + carDir + ".");
+	
+	}
+	
+	private void transmission(int shifter) {
+	
+	carGear = shifter;
+	
+	System.out.println("Car is now in gear " + carGear + ".");
+	
+	}
+	
+	private void gasPedal(int distance) {
+	
+		int newCarX = 0;
+		int newCarY = 0;
+	
+		if (carDir == "north") {
+		
+			if (carGear > 0) {
+				newCarY = carY - distance;
+			} else if (carGear < 0) {
+				newCarY = carY + distance;
+			}
+		
+		} else if (carDir == "south") {
+		
+			if (carGear > 0) {
+				newCarY = carY + distance;
+			} else if (carGear <0) {
+				newCarY = carY - distance;
+			}
+		
+		} else if (carDir == "east") {
+		
+			if (carGear > 0) {
+				newCarX = carY + distance;
+			} else if (carGear < 0) {
+				newCarX = carY - distance;
+			}
+		
+		} else if (carDir == "west") {
+		
+			if (carGear > 0) {
+				newCarX = carY - distance;
+			} else if (carGear < 0) {
+				newCarX = carY + distance;
+			}
+		
+		}
+		
+		carX = newCarX;
+		carY = newCarY;
+		
+		System.out.println("Car location is (" + carX + "," + carY + ").");
+	
+	}
+	
+	static boolean isInteger(String str, int baseTen) {
+	
+		Scanner input = new Scanner(str.trim());
+		if(!input.hasNextInt(baseTen)) return false;
+		input.nextInt(baseTen);
+		return !input.hasNext();
 	
 	}
 
