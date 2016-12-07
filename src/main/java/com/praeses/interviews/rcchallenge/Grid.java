3e7c55ca778f71947c38a11a87c5ab7aeddcebb8
ZacheryLange <Konnitos1@yahoo.com>
2016-12-07 02:04:11 -0500

    Add files via upload
    This is the work I have done for my interview project which I coded in eclipse neon . I was able to download this and ran compiled project from my windows CMD with a gradlew build then a gradlew run command

---
+++ src/main/java/com/praeses/interviews/rcchallenge/Grid.java
@@ -0,0 +1,262 @@
+package com.praeses.interviews.rcchallenge;
+
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Grid extends JPanel {// this is the constructor for road object int the main method and is the panael whre most of the action happens
+	static final int width=500,height=500; 
+	public int move=0;// this is used to stop the car just doing a one 180 with out moving 
+	public int stringBreak=0;// once again this is used to see if the string command stops mid way thought because of a collison between the car and a block
+	private boolean obstaclesOn =false; 
+	private Car theCar = new Car(0,0,20,"S");// the car object is created here 
+	private obstacle block; 
+	private ArrayList<obstacle> randomCorse= new ArrayList<obstacle>(); // much like in when running from a string of commands this array list holds all of the obstacles
+    public Grid(){
+    	setPreferredSize(new Dimension(width,height));
+    }
+    public void paint(Graphics g){// this is the paint method that is called once when program starts and recalled when the image needs to be updated
+    	int x=0;
+    	int y=0;
+    	g.clearRect(0, 0, width, height);// this line clears the road object so there are no leftover images from past car or obstacles. 
+        for (x=0;x<width/20;x++){
+    		for (y=0;y<height/20;y++){
+    		    g.drawRect(x*20, y*20, 20, 20);// this for loop makes the grid 
+    		}
+    	}
+        theCar.drawCar(g);// simply puts the car on the grid
+        if (obstaclesOn= true) {// the obstacles are optional so they are not redrawn unless they are in use 
+        	int i;
+        	for (i=0;i<randomCorse.size();i++){// loop that paints all of the randomly generated obstacles 
+        		randomCorse.get(i).drawBlock(g);// drawing said obstacles
+        	}
+        }
+    }
+    public void carFoward(){// this method is called when the car needs to move forward
+    	String carfacing = new String(theCar.getfacing());// this method works by looking at the cars current facing 
+    	int y=this.theCar.gety();// then increasing the x or y values of the car but before committing the changes it will check a head to see if there is a block in the way 
+    	int x=this.theCar.getx();
+    	this.move=0;
+    	this.stringBreak=0;
+		switch (carfacing ){
+		case "S":// set up for when the car is facing south 
+			y++;
+				if (y>24)
+					y=0;
+			this.theCar.sety(y);
+				if (checkLegalMove()== false){
+					this.stringBreak=1;// this value is used to break out of the string command 
+					y--;
+						if (y<0)// this check here is what causes the wrapping from one side of the grid to the other
+							y=24;
+					this.theCar.sety(y);
+				}
+			repaint();
+		    break;
+		case "N":// slightly different then for south 
+			y--;
+				if (y<0)
+					y=24;
+			this.theCar.sety(y);
+			if (checkLegalMove()== false){
+				this.stringBreak=1;
+				y++;
+					if (y>24)
+						y=0;
+				this.theCar.sety(y);
+			}
+			repaint();
+		    break;
+		case "W": // The set up for moving forward when facing west
+			x--;
+				if (x<0)
+					x=24;
+			this.theCar.setx(x);
+			if (checkLegalMove()== false){
+				this.stringBreak=1;
+				x++;
+					if (x>24)
+						x=0;
+				this.theCar.setx(x);
+			}
+			repaint();
+		    break;
+		case "E": // the set up for moving forward when facing west 
+			x++;
+			if (x>24)
+				x=0;
+			this.theCar.setx(x);
+			if (checkLegalMove()== false){
+				this.stringBreak=1;
+				x--;
+					if (x<0)
+						x=24;
+				this.theCar.setx(x);
+			}
+			repaint();
+		    break;
+		}
+    }
+	public void carBack(){// same as the method that was made for moving forward but is inversed 
+	    	String carfacing = theCar.getfacing();
+	    	int y=this.theCar.gety();
+	    	int x=this.theCar.getx();
+	    	this.move=0;
+	    	this.stringBreak=0;
+			switch (carfacing ){
+			case "S": 
+				y--;
+				if (y<0)
+					y=24;
+				this.theCar.sety(y);
+				if (checkLegalMove()== false){
+					this.stringBreak=1;
+					y++;
+						if (y>24)
+							y=0;
+					this.theCar.sety(y);
+				}
+				repaint();
+			    break;
+			case "N": 
+				y++;
+				if(y>24)
+					y=0;
+				this.theCar.sety(y);
+				if (checkLegalMove()== false){
+					this.stringBreak=1;
+					y--;
+						if (y<0)
+							y=24;
+					this.theCar.sety(y);
+				}
+				repaint();
+			    break;
+			case "W": 
+				x++;
+				if(x>24)
+					x=0;
+				this.theCar.setx(x);
+				if (checkLegalMove()== false){
+					this.stringBreak=1;
+					x--;
+						if (x<0)
+							x=24;
+					this.theCar.setx(x);
+				}
+				repaint();
+			    break;
+			case "E": 
+				x--;
+				if(x<0)
+					x=24;
+				this.theCar.setx(x);
+				if (checkLegalMove()== false){
+					this.stringBreak=1;
+					x++;
+						if (x>24)
+							x=0;
+					this.theCar.setx(x);
+				}
+				repaint();
+			    break;
+			}		
+    }
+    public void carLeft(){// this method checked the facing of the car and then changed it to the facing to the left
+          String carfacing = theCar.getfacing();
+          if (this.move ==0){
+        	  this.move=1;// the move is changed here so as to not let the car turn 180 degrees with out moving at least on space 
+        	  switch (carfacing){
+        	  	case ("N"):
+        	  		theCar.setfacing("W");
+        	  		break; 
+        	  	case ("S"):
+        	  		theCar.setfacing("E");
+        	  		break; 
+        	  	case ("E"):
+        	  		theCar.setfacing("N");
+        	  		break; 
+        	  	case ("W"):
+        	  		theCar.setfacing("S");
+          	  		break; 
+          	}
+          }
+          else 
+        	  JOptionPane.showMessageDialog( Grid.this, String.format("Can not turn twice in a row"));
+    }
+    public void carRight(){// same as carLeft but to the right instead 
+        String carfacing = theCar.getfacing(); 
+        if (this.move ==0){
+        	this.move=1; 
+        	switch (carfacing){
+        		case ("N"):
+      	  			theCar.setfacing("E");
+        	  		break; 
+        		case ("S"):
+      	  			theCar.setfacing("W");
+        	  		break; 
+        		case ("E"):
+      	  			theCar.setfacing("S");
+        	  		break; 
+        		case ("W"):
+      	  			theCar.setfacing("N");
+        	  		break; 
+        	}
+        }
+        else 
+      	  JOptionPane.showMessageDialog( Grid.this, String.format("Can not turn twice in a row"));
+    }
+    public void generateObstacles(){// this method makes random values for the x an y values of the obstacles 
+    	 this.obstaclesOn=true;// in this case i am only doing 100 but the button could be pressed for more of them 
+    	 int i=0;
+    	 for (i=0;i<100;i++){
+    		 int ranX= (int) (Math.random()*25);
+    		 int ranY= (int) (Math.random()*25);
+    		 if (ranX==theCar.getx()){
+    			 if (ranY==theCar.gety()){// this if statement is to stop the obstacles from spawning on the car
+    				 i--; 
+    				 continue;
+    			 }
+    		 }
+         block=new obstacle(ranX,ranY,20);
+         randomCorse.add(block);// the new block added to the array list 
+    	 }
+    	 repaint();// called so that the new obstacles can be added to the grid 
+    }
+    public void destroyObstacles(){// this methods clears all of the blocks objects made and then repaints the screen
+    	randomCorse.clear();
+    	this.obstaclesOn=false; 
+    	repaint(); 
+    }
+    public boolean checkLegalMove(){// this method compares the next move of the car with all of the generated blocks 
+    	boolean legal=true;// if even one matches the loop is broken and the car is reverted to right before it made that move
+    	int i; 
+    	for (i=0;i<randomCorse.size();i++){
+    		if (randomCorse.get(i).getx()==theCar.getx() && randomCorse.get(i).gety()==theCar.gety()){
+    				legal=false; 
+    				JOptionPane.showMessageDialog( Grid.this, String.format("The car can not go here turn back or pick a different starting palce"));
+    				break;
+    		}
+    	}
+		return legal; 
+    }
+    public void initalCar(int x,int y,String facing){// as the name states this sets the starting point of the car
+    	theCar.setx(x);// the values passed to this are generated from user input into text fields then passed here to be set 
+    	theCar.sety(y);
+    	theCar.setfacing(facing);
+    		if (checkLegalMove()==false){
+    		    theCar.setx(0);
+    		    theCar.sety(0);
+    		    theCar.setfacing("N");
+    		}
+    	repaint();// after the car has changed its values its is redrawn at its new location 
+    }
+	public String facingLabel(){// this method allows the main class to have access to the facing through this method 
+    	return theCar.getfacing();// main can access the grid and the grid has access to the car 
+    }
+    
+  }
+
--- src/main/java/com/praeses/interviews/rcchallenge/Main.java
+++ src/main/java/com/praeses/interviews/rcchallenge/Main.java
@@ -1,7 +1,236 @@
 package com.praeses.interviews.rcchallenge;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+/* 
+ * Rc car Challenge
+ * By Zachery Lange
+ * for Praeses interview homework
+ * 
+ * this program use the main class as the frame with the grid object acting as a panel to be put into the main frame
+ * the other two objects are drawn on the panel that is in the main frame
+ * the car class being the rc car and the obstacle class making the walls for the rc car avoid
+ */
+public class Main extends JFrame {
+	private JButton b1,b2,b3,b4,b5,b6,b7,b8;
+	private JTextField tfix,tfiy,tfif,tfStringCommand;
+	private Grid road=new Grid(); // this is the panel where most of the action takes place 
+	private JLabel carFacing,iniX,iniY,iniF,stringCommandLabel;
+	public Main (){// start of the constructor of the main frame 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("RC Car");
+		setResizable(false); 
+		setLayout(new FlowLayout());// the frame is flow layout becuase i use other layouts to do fine work on the panels
+		add(road);
+		JPanel buttonPanel= new JPanel(new GridBagLayout()); // this is the panel is with all of the buttons on it 
+		GridBagConstraints buttonC=new GridBagConstraints();// this this makes grid for me to place all of the buttons on 
+		carFacing= new JLabel("Car is facing South"); // creating all of the buttons and labels while then putting them on the correct panels
+		buttonC.gridx=0;// the button names and label names are mostly speak for themselves
+		buttonC.gridy=0;
+		buttonPanel.add(carFacing, buttonC);
+		b1= new JButton("Foward");
+		buttonC.gridx=0;
+		buttonC.gridy=1;
+		buttonPanel.add(b1, buttonC);
+		b2= new JButton("Back");
+		buttonC.gridx=0;
+		buttonC.gridy=2;
+		buttonPanel.add(b2, buttonC);
+		b3= new JButton("Turn Left");
+		buttonC.gridx=0;
+		buttonC.gridy=3;
+		buttonPanel.add(b3, buttonC);
+		b4= new JButton("Turn Right");
+		buttonC.gridx=0;
+		buttonC.gridy=4;
+		buttonPanel.add(b4, buttonC);
+		b7= new JButton("Set inital Car and Facing");
+		buttonC.gridx=0;
+		buttonC.gridy=5;
+		buttonPanel.add(b7, buttonC);
+		tfix= new JTextField(2);
+		buttonC.gridx=1;
+		buttonC.gridy=6;
+		buttonPanel.add(tfix, buttonC);
+		iniX= new JLabel("Inital X =");
+		buttonC.gridx=0;
+		buttonC.gridy=6;
+		buttonPanel.add(iniX, buttonC);
+		tfiy= new JTextField(2);
+		buttonC.gridx=1;
+		buttonC.gridy=7;
+		buttonPanel.add(tfiy, buttonC);
+		iniY= new JLabel("Inital Y =");
+		buttonC.gridx=0;
+		buttonC.gridy=7;
+		buttonPanel.add(iniY, buttonC);
+		tfif= new JTextField(1);
+		buttonC.gridx=1;
+		buttonC.gridy=8;
+		buttonPanel.add(tfif, buttonC);
+		iniF= new JLabel("Inital Facing =");
+		buttonC.gridx=0;
+		buttonC.gridy=8;
+		buttonPanel.add(iniF, buttonC);
+		b5= new JButton("Make 100 random obstacles");
+		buttonC.gridx=0;
+		buttonC.gridy=9;
+		buttonPanel.add(b5, buttonC);
+		b6= new JButton("Clear obstacles");
+		buttonC.gridx=0;
+		buttonC.gridy=10;
+		buttonPanel.add(b6, buttonC);
+		stringCommandLabel= new JLabel("Enter commands for the Car (f,b,l,r)"); 
+		buttonC.gridx=0;
+		buttonC.gridy=11;
+		buttonPanel.add(stringCommandLabel, buttonC);
+		tfStringCommand= new JTextField(25);
+		buttonC.gridx=0;
+		buttonC.gridy=12;
+		buttonPanel.add(tfStringCommand, buttonC);
+		b8= new JButton("Run String");
+		buttonC.gridx=0;
+		buttonC.gridy=13;
+		buttonPanel.add(b8, buttonC);
+		add(buttonPanel);// end of the grand block of button, text field, and label creation 
+		pack();
+		responceToClick handler = new responceToClick();// making the object that allows the buttons to work when hit 
+		b1.addActionListener(handler);
+		b2.addActionListener(handler); 
+		b3.addActionListener(handler);
+		b4.addActionListener(handler);
+		b5.addActionListener(handler);
+		b6.addActionListener(handler);
+		b7.addActionListener(handler);
+		b8.addActionListener(handler);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		JOptionPane.showMessageDialog( Main.this, String.format("Welcome to the Rc Car the buttons should speak for themselves.\n "
+				+ "Things to note though would be  that the facing text feild requires an uppercase letter and the text field for\n"
+				+ "the string command is in lower case with f being foward, b being back, l being turn left, r being\n"
+				+ "turn right and spces are ignored"));// a small intro to the program built around the idea that random user is running the program
+	}
+	public void foward(){// most of these methods are links to the grid object so as to have power over the other objects that are only in this grid object
+		this.road.carFoward();
+	}
+	public void back(){
+		this.road.carBack();
+	}
+	public void turnLeft(){
+		this.road.carLeft();
+		updatefacinglabel();
+	}
+	public void turnRight(){
+		this.road.carRight();
+		updatefacinglabel();
+	}
+	public void makeObstacles(){
+		this.road.generateObstacles();
+	}
+	public void clearObstacles(){
+		this.road.destroyObstacles();
+	}
+	public void setCar(){// this methods takes the information from the correct text fields to inform the car object in the road object 
+		int initalX=Integer.parseInt(tfix.getText());
+		int initalY=Integer.parseInt(tfiy.getText());
+		String initalFacing= tfif.getText();
+		this.road.initalCar(initalX,initalY,initalFacing);
+		updatefacinglabel();
+	}
+	public void runningString(){// this methods takes the text from the tfStringCommand text field then breaks it up into an array and then runs through the array 
+		String input= tfStringCommand.getText(); //moving the car with each token in the array only stopping with the array is spanned or the car hit a block
+		String[] tokens= input.split("");
+		for (String token:tokens){
+		      if (token.equals("f")){
+		    	foward(); 
+		      	}
+		      else if (token.equals("b")){
+			    back(); 
+		      }
+		      else if (token.equals("l")){
+		    	turnLeft(); 
+		      }
+		      else if (token.equals("r")){
+				turnRight(); 
+		      }
+		      else if (token.equals(" ")){
+		    	  continue; 
+		      }
+		      else {
+		    	  JOptionPane.showMessageDialog( Main.this, String.format("This is not a Proper string of actions ending at last valid move"));
+		    	  break;
+		      }
+		      if (road.stringBreak==1){// this values is what cause the string to stop when it gets changed in the road object
+		    	break;
+		      }
+		 }
     }
+	public void updatefacinglabel(){// this method gets the current facing of the car object to then update label that infroms the user the current facing of the car
+		String currentFacing =this.road.facingLabel(); 
+		switch (currentFacing){
+		case ("N"):
+			this.carFacing.setText("Car is facing North");
+			break;
+		case ("S"):
+			this.carFacing.setText("Car is facing South");
+			break;
+		case ("W"):
+			this.carFacing.setText("Car is facing West");
+			break;
+		case ("E"):
+			this.carFacing.setText("Car is facing East");
+			break;
+		}
+	}
+	public class responceToClick implements ActionListener
+     {
+     	public void actionPerformed (ActionEvent event)
+     	{
+     		String whatButton= new String (event.getActionCommand());
+// so this objest allows my buttons to call methods when they are pressed 
+     			switch(whatButton){
+     			case "Foward":
+     				foward();
+     				break; 
+     			case "Back":
+     				back();
+     				break; 
+     			case "Turn Left":
+     				turnLeft(); 
+     				break; 
+     			case "Turn Right":
+     				turnRight();
+     				break; 
+     			case "Make 100 random obstacles":
+     				makeObstacles();
+     				break;
+     			case "Clear obstacles":
+     				clearObstacles();
+     				break;
+     			case "Set inital Car and Facing":
+     				setCar();
+     				break;
+     			case "Run String":
+     				runningString(); 
+     				break; 
+     		    default: 
+     			}
+     		
+     	}
+     }
+	
+	public static void main(String[] args) {
+		new Main();// and to think all of this is started by just making a new mian. 
+	}
+
 }
